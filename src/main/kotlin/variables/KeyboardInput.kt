4599d412b05d81526be227674167f2247f93d012
Junior Plantier <junior.plantier@gmail.com>
2024-01-22 23:46:27 -0300

    add scanner

---
+++ src/main/kotlin/variables/KeyboardInput.kt
@@ -0,0 +1,28 @@
+package variables
+
+import java.util.*
+
+fun main() {
+
+//    print("Digite o seu nome: ")
+//    val name = readln()
+//
+//    println("Seu nome invertido é: ${name.reversed()}")
+
+//    print("> ")
+//    val n1 = readln().toInt()
+//
+//    print("> ")
+//    val n2 = readln().toInt()
+//
+//    println("Result: ${n1 + n2}")
+
+    val scanner = Scanner(System.`in`)
+    print("> ")
+    val n1 = scanner.nextInt()
+
+    print("> ")
+    val n2 = scanner.nextInt()
+
+    println("Result: ${n1 + n2}")
+}
