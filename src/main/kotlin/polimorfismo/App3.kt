ec2e0541ccb0a9f1eea667d4977b6c2a1364c38d
Junior Plantier <junior.plantier@gmail.com>
2024-11-16 10:48:57 -0300

    smart casting

---
+++ src/main/kotlin/polimorfismo/App3.kt
@@ -0,0 +1,33 @@
+package polimorfismo
+
+fun main() {
+    val a: Animal = Dog()
+    a.eat()
+
+    if(a is Dog) {
+        a.run()
+        val b = a as Dog
+    }
+    
+}
+
+open class Animal() {
+
+    fun eat() {
+        println("eating...")
+    }
+}
+
+class Dog: Animal() {
+
+    fun run() {
+        println("runnning...")
+    }
+}
+
+class Cat: Animal() {
+
+    fun sleep() {
+        println("sleeping...")
+    }
+}
