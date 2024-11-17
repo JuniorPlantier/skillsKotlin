e8886357c433cfad86019f4e1e14492865442eb8
Junior Plantier <junior.plantier@gmail.com>
2024-11-17 11:37:49 -0300

    classe anonima

---
+++ src/main/kotlin/polimorfismo/App5.kt
@@ -0,0 +1,17 @@
+package polimorfismo
+
+// classes anônimas
+
+fun main() {
+
+    val v = object: Vehicle {
+        override fun drive() {
+            println("Driving...")
+        }
+    }
+    v.drive()
+}
+
+interface Vehicle {
+    fun drive()
+}
